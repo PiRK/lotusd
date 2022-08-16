hughhn/bitcoin

Add a --descriptors option to various tests
Adds the option to use a descriptor wallet to:
* wallet_basic.py
* wallet_encryption.py
* wallet_keypool.py
* wallet_keypool_topup.py
* wallet_labels.py

Also runs these tests with --descriptors in test_runner

test/functional/wallet_labels.py
@@ -22,9 +22,16 @@ class WalletLabelsTest(BitcoinTestFramework):
     def skip_test_if_missing_module(self):
         self.skip_if_no_wallet()
 
+    def add_options(self, parser):
+        parser.add_argument('--descriptors', action='store_true', dest="descriptors", help="Run test using a descriptor wallet", default=False)
+
     def run_test(self):
-        # Check that there's no UTXO on the node
         node = self.nodes[0]
+        if self.options.descriptors:
+            node.createwallet(wallet_name='desc', descriptors=True)
+            node.unloadwallet('')
+
+        # Check that there's no UTXO on the node
         assert_equal(len(node.listunspent()), 0)
 
         # Note each time we call generate, all generated coins go into
@@ -113,15 +120,16 @@ class WalletLabelsTest(BitcoinTestFramework):
             assert_raises_rpc_error(-11, "No addresses with label", node.getaddressesbylabel, "")
 
         # Check that addmultisigaddress can assign labels.
-        for label in labels:
-            addresses = []
-            for x in range(10):
-                addresses.append(node.getnewaddress())
-            multisig_address = node.addmultisigaddress(5, addresses, label.name)['address']
-            label.add_address(multisig_address)
-            label.purpose[multisig_address] = "send"
-            label.verify(node)
-        node.generate(101)
+        if not self.options.descriptors:
+            for label in labels:
+                addresses = []
+                for x in range(10):
+                    addresses.append(node.getnewaddress())
+                multisig_address = node.addmultisigaddress(5, addresses, label.name)['address']
+                label.add_address(multisig_address)
+                label.purpose[multisig_address] = "send"
+                label.verify(node)
+            node.generate(101)
 
         # Check that setlabel can change the label of an address from a
         # different label.

PiRK/lotusd

Summary:
> Adds a --descriptors option globally to the test framework. This will
> make the test create and use descriptor wallets. However some tests may
> not work with this.
>
> Some tests are modified to work with --descriptors and run with that
>  option in test_runer:
> * wallet_encryption.py
> * wallet_keypool.py
> * wallet_keypool_topup.py
> * wallet_labels.py
> * wallet_avoidreuse.py
This is a backport of [[https://github.com/bitcoin/bitcoin/pull/16528 | core#16528]] [43a/43]
https://github.com/bitcoin/bitcoin/pull/16528/commits/223588b1bbc63dc57098bbd0baa48635e0cc0b82
I'm splitting this commit into multiple commits to make review easier.
This first part contains everything that works more or less out of the
box and does not require significant changes to work with our codebase.
All the deviations from the original PR in this revision are explained by out of order
backports and lack of segwit and bech32. For instance, changes to key.py
were already included in D8475 & D9934. Deviations in `RPCOverloadWrapper.createwallet`
are explained by D10185 and D9101.
I excluded specifically 3 tests that require changes to transaction
amounts and fees when descriptors are used. With our codebase,
descriptor wallets generate different fees, because in some situations
the legacy wallet generates legacy p2pkh outputs and descriptor wallets generate
`scriptPubKey` / `p2sh` outputs. I will address these in one or two
separate diffs.
These excluded tests are:
 - rpc_psbt.py
 - wallet_basic.py
 - wallet_keypool.py
Test Plan: `ninja check-functional`
Reviewers: #bitcoin_abc, Fabien
Reviewed By: #bitcoin_abc, Fabien
Subscribers: Fabien
Differential Revision: https://reviews.bitcoinabc.org/D10213

test/functional/wallet_labels.py
@@ -126,16 +126,17 @@ class WalletLabelsTest(BitcoinTestFramework):
                                     "")
 
         # Check that addmultisigaddress can assign labels.
-        for label in labels:
-            addresses = []
-            for _ in range(10):
-                addresses.append(node.getnewaddress())
-            multisig_address = node.addmultisigaddress(
-                5, addresses, label.name)['address']
-            label.add_address(multisig_address)
-            label.purpose[multisig_address] = "send"
-            label.verify(node)
-        node.generate(101)
+        if not self.options.descriptors:
+            for label in labels:
+                addresses = []
+                for _ in range(10):
+                    addresses.append(node.getnewaddress())
+                multisig_address = node.addmultisigaddress(
+                    5, addresses, label.name)['address']
+                label.add_address(multisig_address)
+                label.purpose[multisig_address] = "send"
+                label.verify(node)
+            node.generate(101)
 
         # Check that setlabel can change the label of an address from a
         # different label.